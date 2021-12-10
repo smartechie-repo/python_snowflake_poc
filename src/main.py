b359307f7db0524896e8ed687a615759b704dcfb
Sudhir Ranjan Pradhan <sudhir.pradhan@leeyo.com>
2021-12-10 13:06:30 -0800

    Added implementation for SqlAlchemiy  way of connecting to snowflake and read table

--- src/main.py
+++ src/main.py
@@ -1,14 +1,31 @@
 import json
 from utils.helper import init_environment
 from connectors.snowflaker import SnowflakeConnector
+from connectors.alchemic import sqlalchemy_snowflake_connector, sqlalchemy_snowflake_connector_with
 
+def load_using_snowflake_sqlalchemy(cred: dict, metadata: dict):
 
-def load_using_snowflake_connector(cred: dict):
+    test_table_name = metadata['tables'][0]
+
+    engine = sqlalchemy_snowflake_connector_with(cred=cred, warehouse=metadata["warehouse"], \
+        database=metadata["database"], schema=metadata["schema"], role="sysadmin")
+    connection = engine.connect()
+
+    try:
+        # Read the table
+        result = connection.execute(f"SELECT * FROM {test_table_name}")
+        for row in result:
+            print(row)
+    finally:
+        connection.close()
+        engine.dispose()
+
+def load_using_snowflake_connector(cred: dict, metadata: dict):
     conn = SnowflakeConnector.get_instance(cred)
 
     """
     You can also set session parameters by executing the SQL statement ALTER SESSION SET ... after connecting:
-    con.cursor().execute("ALTER SESSION SET QUERY_TAG = 'dev-sudhir'")
+    con.cursor().execute("ALTER SESSION SET QUERY_TAG = '<some tag name>'")
 
     """
     data = {}
@@ -19,24 +36,24 @@ def load_using_snowflake_connector(cred: dict):
     print("success in connecting")
     conn.cursor().execute("use role sysadmin")
 
-    test_tenant_detail = cred["test"]
-    test_table_name = "DATAVERSE_WHITE_BOARD"
+    test_table_name = metadata['tables'][0]
 
-    init_environment(conn=conn, warehouse=test_tenant_detail["warehouse"], database=test_tenant_detail["database"], \
-        schema=test_tenant_detail["schema"], tables=list(test_table_name))
+    init_environment(conn=conn, warehouse=metadata["warehouse"], database=metadata["database"], \
+        schema=metadata["schema"], tables_info=metadata["meta_info"])
+
     # Creating Tables and Inserting Data
     conn.cursor().execute(
-        '''
-        insert into R2_DATAVERSE_WHITE_BOARD 
+        f'''
+        insert into {test_table_name} 
             values ('T000011', 'srp', 'SFO', '94016', CURRENT_DATE(), CURRENT_DATE())
         ''')
 
 
     # Putting Data
-    conn.cursor().execute(f'PUT file://./data/speakers* @{test_tenant_detail["database"]}.{test_tenant_detail["schema"]}.%{test_table_name}')
+    conn.cursor().execute(f'PUT file://./data/speakers* @{metadata["database"]}.{metadata["schema"]}.%{test_table_name}')
 
 
-    conn.cursor().execute(f"COPY INTO {test_table_name} FROM @{test_tenant_detail['database']}.{test_tenant_detail['schema']}.%{test_table_name}/speakers.csv.gz \
+    conn.cursor().execute(f"COPY INTO {test_table_name} FROM @{metadata['database']}.{metadata['schema']}.%{test_table_name}/speakers.csv.gz \
                         file_format = (type = csv field_delimiter=',')  pattern = '.*.csv.gz' on_error= 'skip_file'")
 
     # For S3
@@ -84,10 +101,14 @@ def load_using_snowflake_connector(cred: dict):
 
 
 def main():
-    with open("cred.json","r") as f:
-        cred = json.load(f)
+    with open("cred.json","r") as c:
+        cred: dict = json.load(c)
+    
+    with open("metadata.json","r") as m:
+        metadata: dict = json.load(m)
 
-    load_using_snowflake_connector(cred)
+    #load_using_snowflake_connector(cred, metadata = metadata)
+    load_using_snowflake_sqlalchemy(cred=cred, metadata = metadata)
 
 if __name__ == "__main__":
     main()
